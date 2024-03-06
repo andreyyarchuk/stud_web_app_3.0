2cdc2e107c2fdba81b23e247af325d89a473b368
andreyyarchuk <dron4432@gmail.com>
2024-03-06 09:21:32 +0200

    3.0.0: add routes (get, get/id, delate, post)

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const express = require("express")
+require('dotenv').config()
+
+const express = require('express')
 
 const app = express()
+// Database
+const articles = [{title: "Example"}]
 
-app.get("/", (req, res) => {
-    res.send("Hello word")
+app.set('port', process.env.PORT || 3001)
+
+
+
+// Routers
+
+app.get('/articles', (req, res, next) => {
+    res.send(articles)
 })
 
-app.listen(3000, () =>  {
-    console.log("Server is wowrking")
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id
+    console.log('Fetching:', id)
+    res.send(articles[id])
 })
+
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id
+    console.log('Deleting:', id)
+    delete articles[id]
+    res.send({message: "Deleted"})
+})
+
+app.post('/articles', (req, res, next) => {
+    res.send("ok")
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Web app available at http://127.0.0.1:${app.get('port')}`)
+})
+    
+module.exports = app
